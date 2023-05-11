3f58dc455e700296c6cfcea087f9cabf1fa7f890
양원준 <anyas1398@gmail.com>
2023-05-11 23:50:28 +0900

    Update UserService.java

--- src/main/java/com/group/libraryapp/service/user/UserService.java
+++ src/main/java/com/group/libraryapp/service/user/UserService.java
@@ -2,6 +2,7 @@ package com.group.libraryapp.service.user;
 
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
 import com.group.libraryapp.repository.user.UserRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -32,5 +33,9 @@ public class UserService {
 
     public void saveUser(UserCreateRequest request) {
         userRepository.saveUser(request.getName(), request.getAge());
+    }
+
+    public list<UserResponse> getUsers() {
+        userRepository.getUsers();
     }
 }
