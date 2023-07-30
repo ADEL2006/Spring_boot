dac7511531362188347726fa61770ca6c750c3a7
양원준 <anyas1398@gmail.com>
2023-07-31 03:27:31 +0900

    .

--- src/main/java/com/group/libraryapp/repository/book/BookMemoryRepository.java
+++ src/main/java/com/group/libraryapp/repository/book/BookMemoryRepository.java
@@ -1,5 +1,8 @@
 package com.group.libraryapp.repository.book;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BookMemoryRepository implements BookRepository {
 
     //private final List<Book> books = new ArrayList<Book>();
