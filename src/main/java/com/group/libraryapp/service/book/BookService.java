605b7ddc9e6e683b39bf0527dc8f0da2c01fd657
양원준 <anyas1398@gmail.com>
2023-07-31 03:37:35 +0900

    Update BookService.java

--- src/main/java/com/group/libraryapp/service/book/BookService.java
+++ src/main/java/com/group/libraryapp/service/book/BookService.java
@@ -1,7 +1,5 @@
 package com.group.libraryapp.service.book;
 
-import com.group.libraryapp.repository.book.BookMemoryRepository;
-import com.group.libraryapp.repository.book.BookMySqlRepository;
 import com.group.libraryapp.repository.book.BookRepository;
 import org.springframework.stereotype.Service;
 
