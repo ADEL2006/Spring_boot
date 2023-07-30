ad664627e50761ad80776a0759db6ffe68fa7980
양원준 <anyas1398@gmail.com>
2023-07-31 03:31:08 +0900

    Update BookMySqlRepository.java

--- src/main/java/com/group/libraryapp/repository/book/BookMySqlRepository.java
+++ src/main/java/com/group/libraryapp/repository/book/BookMySqlRepository.java
@@ -1,7 +1,8 @@
 package com.group.libraryapp.repository.book;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
-
+@Primary
 @Repository
 public class BookMySqlRepository implements BookRepository {
     @Override
