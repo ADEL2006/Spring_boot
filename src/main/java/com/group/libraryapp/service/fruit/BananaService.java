c00ca7faec3ad476ae06764ea7d3838666f88b16
양원준 <anyas1398@gmail.com>
2023-08-02 23:00:45 +0900

    Update BananaService.java

--- src/main/java/com/group/libraryapp/service/fruit/BananaService.java
+++ src/main/java/com/group/libraryapp/service/fruit/BananaService.java
@@ -4,6 +4,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
-@Qualifier("main")
+@Qualifier("main") // 직접 특별히 지정하는것은 우선순위가 높다.
 public class BananaService implements FruitService{
 }
