fab1ee4cbb0ca2c20a8f23da67dd696512f496c3
양원준 <anyas1398@gmail.com>
2023-08-03 23:55:14 +0900

    Update BookMemoryRepository.java

--- src/main/java/com/group/libraryapp/repository/book/BookMemoryRepository.java
+++ src/main/java/com/group/libraryapp/repository/book/BookMemoryRepository.java
@@ -3,6 +3,7 @@ package com.group.libraryapp.repository.book;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
+// Primary: 우선적으로 실행
 @Primary
 @Repository
 public class BookMemoryRepository implements BookRepository {
