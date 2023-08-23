57e7cf6289c9ff6d7ce08213b84558e02a1ebf69
양원준 <anyas1398@gmail.com>
2023-08-23 20:19:55 +0900

    Update User.java

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -15,6 +15,8 @@ public class User {
     private String name;
     private Integer age;
 
+    protected User() {}
+
     public User(String name, Integer age) {
         if(name == null || name.isBlank()){
             throw new IllegalArgumentException(String.format("잘못된 name(%s)이 들어 왔습니다.", name));
